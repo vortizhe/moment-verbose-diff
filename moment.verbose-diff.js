f4ae24a3b06889706405c0dc28218bef1c9dd1a3
Victor Ortiz <kespers@gmail.com>
2015-10-06 13:39:15 +0200

    Basic functionality

---
+++ moment.verbose-diff.js
@@ -0,0 +1,38 @@
+(function(moment) {
+  moment.fn.verboseDiff = function(date2) {
+    return moment.verboseDiff(this, date2);
+  };
+
+  moment.verboseDiff = function(date1, date2) {
+    var d1 = moment(date1),
+        d2 = moment(date2);
+
+    if (d1.isSame(d2)) {
+      return 0;
+    } else if (d1.isBefore(d2)) {
+      return false;
+    }
+
+    var dDiff = d1.date() - d2.date(),
+        hDiff = d1.hour() - d2.hour(),
+        minDiff = d1.minute() - d2.minute(),
+        secDiff = d1.second() - d2.second();
+
+    if (secDiff < 0) {
+      secDiff = 60 + secDiff;
+      minDiff--;
+    }
+
+    if (minDiff < 0) {
+      minDiff = 60 + minDiff;
+      hDiff--;
+    }
+
+    return {
+      "days": dDiff,
+      "hours": hDiff,
+      "minutes": minDiff,
+      "seconds": secDiff,
+    };
+  };
+})(moment);
